dc1cc95c2a3d63168517b0e6eb2240fd6e208ec9
Papipeto <dorianfleury@gmail.com>
2017-06-27 14:44:59 +0100

    Copy MockAdapter's behaviour on a classic adapter that would throw a NotConnectedException if no session is existing

--- test/Fixtures/MockAdapter.php
+++ test/Fixtures/MockAdapter.php
@@ -1,28 +1,43 @@
 <?php
 namespace SftpConnectorTest\Fixtures;
 
+use SftpConnector\Libssh2\NotConnectedException;
 use SftpConnector\SftpOptions;
 use SftpConnector\SftpAdapter;
 
 class MockAdapter implements SftpAdapter
 {
+    private $session;
+
     public function connect($host, $post = 22)
     {
+        $this->session = 'foo';
+
         return $this;
     }
 
     public function authenticate(SftpOptions $options)
     {
+        if (!$this->session) {
+            throw new NotConnectedException;
+        }
+
         return $this;
     }
 
     public function fetch($source)
     {
+        if (!$this->session) {
+            throw new NotConnectedException;
+        }
+
         return fopen(__DIR__ . '/fixture.json', 'r');
     }
 
     public function disconnect()
     {
+        $this->session = null;
+
         return $this;
     }
 }
